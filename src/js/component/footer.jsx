17552b8ff46999ffa49b05bbdb9438a3a096d206
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-10-08 23:19:50 +0000

    Falta ajustar el diseño

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <footer className="mt-auto text-white-50">
+    <p className="card-text">Cover template for.</p>
+  </footer>
+    )
+}
+
+export default Footer
